9609cbdb6c3c34fe5f9f1fe658cdc3f360a0a270
cseve123 <cseve123@163.com>
2020-09-24 23:17:17 +0800

    const loader plugin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 const json = require("./index.json");  // commonjs
 import { add } from './other'; // es module
 console.log(json, add(2, 3));
+
+import pic from './media/logo.png';
+import './css/a.css';
+import './css/index.css';
+import './css/index.less';
+
+var img = new Image();
+img.src = pic;
+img.classList.add('logo');
+const root = document.getElementById('root');
+const h1 = document.createElement('h1');
+h1.innerText = 'webpack学习';
+root.append(h1);
+root.append(img);
+console.l();
--- webpack.config.js
+++ webpack.config.js
@@ -51,13 +51,95 @@
 // loader基础形式
 // module.exports = {
 //   module: {
-//     rules: {
+//     rules: [
 //       test: /\.xxx$/,
 //       use: {
 //         loader: 'xxx-loader',
 //         options: {}
 //       }
 
-//     }
+//     ]
 //   }
 // }
+
+// loader plugin 使用
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // html处理
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // 打包清除
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // css单文件处理
+console.log('______________________', process.env.NODE_ENV);
+module.exports = {
+    module: {
+        rules: [
+            // {  // file-loader 处理文件
+            //     test: /\.(png|jpe?g|gif)$/,
+            //     use: {
+            //         loader: "file-loader",
+            //         options: {
+            //             name: "[name]_[hash:8].[ext]",
+            //             outputPath: 'media/'
+            //         }
+            //     }
+            // },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        name: "[name]_[hash:8].[ext]",
+                        limit: 2048,
+                        outputPath: 'media/'
+                    }
+                }
+            },
+            {
+                test: /\.woff2$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name]_[hash:8].[ext]',
+                        outputPath: 'media/'
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            injectType: 'singletonStyleTag'  // 多css合成一个
+                        }
+                    },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            esModule: true
+                        }
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    'less-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'HtmlWebpackPlugin',
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name]_[hash:8].css'
+        })
+    ],
+    devtool: 'nosources-source-map',
+    mode: 'development'
+}
