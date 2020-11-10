b370ea31d814efc32070dcdb03e46caa53d90cef
cseve123 <cseve123@163.com>
2020-11-10 21:53:17 +0800

    no do something

--- webpack.config.js
+++ webpack.config.js
@@ -316,7 +316,7 @@ module.exports = {
             warnings: true,
             errors: true
         },
-        proxy: { // 代理api 返回数据
+        proxy: { // 代理api 返回数据结果
             '/api': {
                 target: 'http://localhost:9092'
             }
